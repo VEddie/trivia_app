539b52c7488290ebb5adaa8c3e0f4741270103dd
VEddie <edijose18@gmail.com>
2025-11-20 23:10:14 -0400

    Added API functionality, question load correctly

--- js/script.js
+++ js/script.js
@@ -1,24 +1,96 @@
 const baseURL = 'https://opentdb.com/api.php?';
+let gameData = null;
 
+// Main Menu
 let nameInput = document.getElementById('playerName');
 let amountInput = document.getElementById('questionAmount');
 let difficultyInput = document.getElementsByName('difficulty');
 let categoryInput = document.getElementsByName('category');
 let startButton = document.getElementById('gameStartButton');
 
-let gameData = null;
+// Game Screen
+let questionText = document.querySelector('.question_text');
+let questionOptions = document.getElementsByClassName('answer');
+let timerParagraph = document.querySelector('.timer_circle');
+let timeRemaining = 20;
+let progressBar = document.querySelector('.progress_bar');
+let progressValue = 0;
+let testButton = document.getElementById('test_button');
 
-const fetchQuestions = (gameConfig) => {
-    let questionURL = baseURL 
-                    + `amount=${gameConfig.amount}&`
-                    + `difficulty=${gameConfig.difficulty}&`
-                    + `type=multiple`;
+let fetchInterval = setInterval(() => {
+    if (gameData === null) return;
 
-    if(gameConfig.category)
+    else {
+        clearInterval(fetchInterval);
+        console.log(gameData);
+        loadNextQuestion();
+    }
+}, 1000);
+
+let fetchQuestions = (gameConfig) => {
+    let questionURL = baseURL
+        + `amount=${gameConfig.amount}&`
+        + `difficulty=${gameConfig.difficulty}&`
+        + `type=multiple`;
+
+    if (gameConfig.category)
         questionURL += `&category=${gameConfig.category}`
 
-    console.log(`API URL: ${questionURL}`);
+    return fetch(questionURL);
 };
+
+let resetTimer = () => {
+    let timerInterval = setInterval(() => {
+        if (timeRemaining === 0) {
+            clearInterval(timerInterval);
+            timeRemaining = 20;
+            timerParagraph.style.backgroundColor = '';
+            timerParagraph.innerText = timeRemaining;
+            return;
+        }
+
+        timeRemaining--;
+        timerParagraph.innerText = timeRemaining;
+
+        if (timeRemaining <= 5)
+            timerParagraph.style.backgroundColor = 'crimson';
+    }, 1000);
+}
+
+let shuffleAnswers = (arr) => {
+    let shuffled = window.structuredClone(arr);
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let k = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = k;
+    }
+
+    return shuffled;
+}
+
+let loadNextQuestion = () => {
+    if (gameData.length === 0) return;
+
+    resetTimer();
+
+    let currentQuestion = gameData.shift();
+    let answers = shuffleAnswers([...currentQuestion.incorrect_answers, currentQuestion.correct_answer]);
+    questionText.innerText = currentQuestion.question;
+
+    for(let i = 0; i < answers.length; i++)
+        questionOptions[i].innerText = answers[i];
+
+    progressValue += (1/questionAmount.value) * 100
+    progressBar.style.width = progressValue + '%';
+
+    let answerTimeout = setTimeout(() => {
+        clearTimeout(answerTimeout);
+        loadNextQuestion();
+    }, 21000);
+}
+
 
 startButton.addEventListener('click', () => {
     let gameConfig = {
@@ -27,76 +99,26 @@ startButton.addEventListener('click', () => {
         score: 0
     };
 
-    for(let i = 0; i < difficultyInput.length; i++) 
-        if(difficultyInput[i].checked) {
+    for (let i = 0; i < difficultyInput.length; i++)
+        if (difficultyInput[i].checked) {
             gameConfig.difficulty = difficultyInput[i].value;
             break;
         }
-    
-    for(let i = 0; i < categoryInput.length; i++) {
-        if(categoryInput[i].checked) {
+
+    for (let i = 0; i < categoryInput.length; i++) {
+        if (categoryInput[i].checked) {
             gameConfig.category = categoryInput[i].value;
             break;
         }
     }
-    
+
     fetchQuestions(gameConfig);
-    
+
 });
 
-let timerParagraph = document.querySelector('.timer_circle');
-let timeRemaining = 20;
-
-let timerInterval = setInterval(() => {
-    if(timeRemaining === 0) {
-        clearInterval(timerInterval);
-        timeRemaining = 20;
-        timerParagraph.style.backgroundColor = '';
-        timerParagraph.innerText = timeRemaining;
-        return;
-    }
-
-    timeRemaining--;
-    timerParagraph.innerText = timeRemaining;
-
-    if(timeRemaining <= 5) 
-        timerParagraph.style.backgroundColor = 'crimson';
-}, 1000)
-
-// API URL:?amount=val&category=val&difficulty=(easy|medium|hard)&type=multiple
-
-// fetchQuestions()
-//     .then(res => res.json())
-//     .then(data => console.log(data.results[0]));
-
-// API response structure:
-// results: array
-    // category: string
-    // correct_answer: string
-    // difficulty: string
-    // incorrect_answers: array
-    // question: string
-        
-
-// Category IDs:
-    // General Knowledge: 9 [X]
-    // Books: 10
-    // Film: 11 [X]
-    // Music: 12 [X]
-    // Musicals & Theatres: 13
-    // TV: 14 
-    // Video Games: 15 [X]
-    // Board Games: 16
-    // Science & Nature: 17
-    // Computers: 18
-    // Mathematics: 19 [X]
-    // Mythology: 20
-    // Sports: 21 [X]
-    // Geography: 22
-    // History: 23
-    // Politics: 24
-    // Art: 25
-    // Celebrities: 26
-    // Animals: 27 [X]
-
-// Difficulty
+testButton.addEventListener('click', () => {
+    const URL = 'https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple&category=15';
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => gameData = data.results);
+});
